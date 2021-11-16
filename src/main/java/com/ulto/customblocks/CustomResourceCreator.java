bfdff9b7b85ab9a55d770c1bc9dbf434c328c4ae
BuildTools <unconfigured@null.spigotmc.org>
2021-11-15 16:25:27 -0800

    Fixed lichen item model

--- src/main/java/com/ulto/customblocks/CustomResourceCreator.java
+++ src/main/java/com/ulto/customblocks/CustomResourceCreator.java
@@ -925,6 +925,7 @@ public class CustomResourceCreator {
 						}
 						break;
 					case "pane":
+					case "lichen":
 						try {
 							FileWriter blockItemModelwriter = new FileWriter(blockItemModel);
 							BufferedWriter blockItemModelbw = new BufferedWriter(blockItemModelwriter);
